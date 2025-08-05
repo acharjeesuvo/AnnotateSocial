9e59e135fb34f1527a20e2a237cea1e52e8ab826
acharjeesuvo <acharjeesuvo.ml@gmail.com>
2025-08-05 20:50:32 +0530

    Initial commit

---
+++ test_supabase_Git.py
@@ -0,0 +1,53 @@
+<<<<<<< HEAD
+import streamlit as st
+import psycopg2
+
+
+SUPABASE_DB_CONFIG = {
+    "dbname": st.secrets["db"]["dbname"],
+    "user": st.secrets["db"]["user"],
+    "password": st.secrets["db"]["password"],
+    "host": st.secrets["db"]["host"],
+    "port": "5432"
+
+}
+
+def connection_check():
+    try:
+        conn = psycopg2.connect(**SUPABASE_DB_CONFIG)
+        cur = conn.cursor()
+        st.header("✅ Connected successfully into Supabase.")
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print("❌ Connection failed:", e)
+
+connection_check()
+
+=======
+import streamlit as st
+import psycopg2
+
+
+SUPABASE_DB_CONFIG = {
+    "dbname": st.secrets["db"]["dbname"],
+    "user": st.secrets["db"]["user"],
+    "password": st.secrets["db"]["password"],
+    "host": st.secrets["db"]["host"],
+    "port": "5432"
+
+}
+
+def connection_check():
+    try:
+        conn = psycopg2.connect(**SUPABASE_DB_CONFIG)
+        cur = conn.cursor()
+        st.header("✅ Connected successfully into Supabase.")
+        cur.close()
+        conn.close()
+    except Exception as e:
+        print("❌ Connection failed:", e)
+
+connection_check()
+
+>>>>>>> aa2f7b47 (Added images folder)
